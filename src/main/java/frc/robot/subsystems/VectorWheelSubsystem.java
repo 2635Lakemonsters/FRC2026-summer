3cdf51c8ae178a51a40b207d337347acaf29e456
Preston-Hegouaburu <preston.hegouaburu@gmail.com>
2026-08-08 11:18:41 -0700

    Vector Wheel Code Changes

--- src/main/java/frc/robot/subsystems/VectorWheelSubsystem.java
+++ src/main/java/frc/robot/subsystems/VectorWheelSubsystem.java
@@ -7,8 +7,26 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VectorWheelSubsystem extends SubsystemBase {
+  TalonFX m_vectorWheelMotor;
+  private VoltageConfigs m_voltageConfig = new VoltageConfigs();
+
   /** Creates a new VectorWheelSubsystem. */
-  public VectorWheelSubsystem() {}
+  public VectorWheelSubsystem() {
+    m_vectorWheelMotor = new TalonFX(Constants.VECTOR_WHEEL_ID);
+    m_vectorWheelMotor.getConfigurator().apply(m_voltageConfig);
+  }
+
+  public void vectorWheelsIn() {
+    m_vectorWheelMotor.setVoltage(Constants.VECTOR_WHEEL_IN_VOLTAGE);
+  }
+
+  public void vectorWheelsOut() {
+    m_vectorWheelMotor.setVoltage(Constants.VECTOR_WHEEL_OUT_VOLTAGE);
+  }
+
+  public void vectorWheelsStop() {
+    m_vectorWheelMotor.setVoltage(0);
+  }
 
   @Override
   public void periodic() {
