f7d9407e2fb10d2c099962a332faaa280917efaa
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2026-08-08 12:34:33 -0700

    Added Intake/Trasport Code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,11 +16,17 @@ public final class Constants {
   
   //Intake Constants
   public static final int INTAKE_MOTOR_ID = 22;
+  public static final int INTAKE_IN_VOLTAGE = 10;
+  public static final int INTAKE_OUT_VOLTAGE = -3;
+  public static final int INTAKE_ANGLE_MOTOR_ID = 11;
 
   //Transport Constants
   public static final int VECTOR_WHEEL_ID = 13;
   public static final int VECTOR_WHEEL_IN_VOLTAGE = -1;
   public static final int VECTOR_WHEEL_OUT_VOLTAGE = 1;
+  public static final int UPTAKE_MOTOR_ID = 0;
+  public static final int UPTAKE_UP_VOLTAGE = -3;
+  public static final int UPTAKE_DOWN_VOLTAGE = 3;
 
 
 
--- src/main/java/frc/robot/commands/VectorWheelCommand.java
+++ src/main/java/frc/robot/commands/VectorWheelCommand.java
@@ -5,25 +5,34 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.VectorWheelSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class VectorWheelCommand extends Command {
+  VectorWheelSubsystem m_vectorWheelSubsystem;
   /** Creates a new VectorWheelCommand. */
-  public VectorWheelCommand() {
+  public VectorWheelCommand(VectorWheelSubsystem vectorWheelSubsystem) {
+    m_vectorWheelSubsystem = vectorWheelSubsystem;
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_vectorWheelSubsystem.setvectorWheelIn();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_vectorWheelSubsystem.setvectorWheelStop();
+  }
 
   // Returns true when the command should end.
   @Override
